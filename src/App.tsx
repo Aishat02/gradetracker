87e6a2a5db804433ac7a3b185ad5aca9cbdf4107
Aris <titilayotijani15@gmail.com>
2025-09-18 14:05:01 +0100

    add react-query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./assets/styles/app.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Home from "./Home";
-import UserAccount from "./UserAccount";
-// Home, Dashboard
+import Home from "@/landingPage/Home";
+import Login from "@/features/authentication/components/Login";
+import Dashboard from "@/dashboard/components/Dashboard";
+import SignUp from "@/features/authentication/components/SignUp";
+import DataFlow from "@/shared/context/DataFlow";
+import AuthStatus from "@/features/authentication/hooks/AuthStatus";
+import { ToastContainer } from "react-toastify";
+import { JSX } from "react";
 
-const App: React.FC = () => {
+export const queryClient = new QueryClient();
+
+const App = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <main className="App container-fluid">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/user-account" element={<UserAccount />} />
-        </Routes>
-      </main>
+      <DataFlow>
+        <QueryClientProvider client={queryClient}>
+          <ToastContainer autoClose={3000} />
+          <AuthStatus />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </QueryClientProvider>
+      </DataFlow>
     </BrowserRouter>
   );
 };
