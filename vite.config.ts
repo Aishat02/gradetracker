cfe96ec5cff3004ae52a877a897403af173ccf72
Aris <titilayotijani15@gmail.com>
2025-09-21 16:00:24 +0100

    update to use port

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["f5tkr3-5173.csb.app"],
+    port: 3000,
   },
   resolve: {
     alias: {
